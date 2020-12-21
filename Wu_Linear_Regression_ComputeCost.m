e83c5dd8c4e2c854d79460c55f68f54e8a15c2cb
Tsung jen Wu (Darren) <54468182+DarrenTsungjenWu@users.noreply.github.com>
2020-12-21 16:03:33 +0800

    Add files via upload

---
+++ Wu_Linear_Regression_ComputeCost.m
@@ -0,0 +1,68 @@
+%Do the preprocessing work
+data = load('D:\machine-learning-ex1\ex1\ex1data1.txt');
+y = data(:, 2);
+m = length(y); % number of training examples
+X = [ones(m, 1), data(:,1)]; % Add a column of ones to x theta = zeros(2, 1); % initialize fitting parameters
+theta = zeros(2, 1); % initialize fitting parameters
+iterations = 1500; alpha = 0.01;
+
+
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+% You need to return the following variables correctly 
+J = 0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+
+h = X*theta;
+sqre = (h - y).^2;
+J = sum(sqre)/2*m;
+
+% =========================================================================
+
+end
+
+
+%==============================================================
+%Sample
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+
+predictions = X*theta;              % predictions of hypothesis on examples
+sqrErrors   = (predictions - y).^2; % squared errors
+
+J = 1/(2*m) * sum(sqrErrors);
+
+
+% =========================================================================
+
+end
+
+%==========================================================
+
+% compute and display initial cost
+J = computeCost(X, y, theta);
+fprintf('With theta = [0 ; 0]\nCost computed = %f\n', J);
+%The cost we expect to get is around 32.07
+
